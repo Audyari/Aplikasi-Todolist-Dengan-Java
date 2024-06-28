fbd70fb35a568384453cf96868ad10cf8099cdca
Audyari Wiyono <audy123ari@gmail.com>
2024-06-28 09:28:12 +0700

    Menentukan Business Logic

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
  https://xd.adobe.com/view/b0d73a96-5829-4dd7-b8df-ad04cd972212-e584/
 
+1. Prototype nya
+2. Bisnis Logic nya
+   
+   - Menambah todo ke list
+   - Mehapus todo dari list
+   - Menampilkan todo list
--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -6,5 +6,30 @@ public class AplikasiTodolist {
 
     public static void main(String[] args) {
 
+
+        /**
+         * Menampilkan todo list
+         */
+        public static void showTodoList () {
+
+        }
+
+        /**
+         * Menambah todo ke list
+         */
+        public static void addTodoList () {
+
+        }
+
+        /**
+         * Mehapus todo dari list
+         */
+        public static boolean removeTodoList() {
+
+        }
+
+
+
+
     }
 }
