95341ae23f2b8b09342a98d5e733844d6d002180
Audyari Wiyono <audy123ari@gmail.com>
2024-08-11 21:56:37 +0700

    Test Logic 2

--- src/ToDoLits.java
+++ src/ToDoLits.java
@@ -1,49 +1,53 @@
+
 public class ToDoLits {
     public static String[] DataTodoList = new String[5];
 
+    public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
     public static void main(String[] args) {
 
-        // testTampilkanDataTodo("Makan", "Minum", "Tidur");
-        testtambahDataTodo();
+        testtampilkanHapusData();
 
     }
 
-    static void tampilkanDataTodo(String... DataTodoList) {
-        // for (int i = 0; i < DataTodoList.length; i++) {
+    static void tampilkanDataTodo() {
 
-        // var Hasil = DataTodoList[i];
-        // var Urutan = i + 1;
+        for (int i = 0; i < DataTodoList.length; i++) {
+            var ListData = DataTodoList[i];
+            var No = i + 1;
 
-        // if (Hasil != null) {
-        // System.out.println(Urutan + ". " + Hasil);
-        // }
-
-        // }
-        var i = 1;
-        for (String Hasil : DataTodoList) {
-            System.out.println(i + ". " + Hasil);
-            i += 1;
+            if (ListData != null) {
+                System.out.println(No + ". " + ListData);
+            }
         }
+
     }
 
-    static void testTampilkanDataTodo(String... DataTodoList) {
+    static void testTampilkanDataTodo() {
 
-        tampilkanDataTodo(DataTodoList);
+        DataTodoList[0] = "Belajar Java Dasar";
+        DataTodoList[1] = "Studi Kasus Java Dasar : Aplikasi Todolist";
+        tampilkanDataTodo();
     }
 
     static void tambahDataTodo(String Nilai) {
 
-        // Cek Data Sudah Penuh Apa Belum
+        var DataPenuh = true;
         for (int i = 0; i < DataTodoList.length; i++) {
             if (DataTodoList[i] == null) {
+                DataPenuh = false;
                 break;
-            } else {
-                // System.out.println("Data Penuh Tambahkan");
-                var TitipData = DataTodoList;
-                DataTodoList = new String[DataTodoList.length * 10];
-                for (int j = 0; j < TitipData.length; j++) {
-                    DataTodoList[i] = TitipData[i];
-                }
+            }
+        }
+
+        if (DataPenuh) {
+
+            var TitipData = DataTodoList;
+
+            DataTodoList = new String[DataTodoList.length * 2];
+
+            for (int i = 0; i < TitipData.length; i++) {
+                DataTodoList[i] = TitipData[i];
             }
 
         }
@@ -51,7 +55,6 @@ public class ToDoLits {
         for (int i = 0; i < DataTodoList.length; i++) {
             if (DataTodoList[i] == null) {
                 DataTodoList[i] = Nilai;
-                System.out.println((i) + ". " + Nilai);
                 break;
             }
 
@@ -59,14 +62,153 @@ public class ToDoLits {
     }
 
     static void testtambahDataTodo() {
-        tambahDataTodo("ASEP");
-        tambahDataTodo("ASEPPPP");
-        tambahDataTodo("AUDYARI");
-        tambahDataTodo("AUDYARI1");
-        tambahDataTodo("AUDYARI11");
+        for (int i = 0; i < 5; i++) {
+            tambahDataTodo("Data Ke = " + i);
+
+        }
+
+        tampilkanDataTodo();
     }
 
-    static void hapusDataTodo() {
+    static boolean hapusDataTodo(int Urutan) {
+
+        if (Urutan >= DataTodoList.length) {
+            return false;
+        }
+
+        if (Urutan == 0) {
+            return false;
+        }
+
+        if (DataTodoList[Urutan] == null) {
+            return false;
+        } else {
+            // Misalkan Urutan = 3
+            int indexToRemove = Urutan - 1; // indeks yang ingin dihapus
+
+            // Pindahkan elemen setelah indeks yang dihapus ke kiri
+            for (int i = indexToRemove; i < DataTodoList.length - 1; i++) {
+                DataTodoList[i] = DataTodoList[i + 1];
+            }
+
+            // Hapus elemen terakhir
+            DataTodoList[DataTodoList.length - 1] = null;
+
+            return true;
+        }
 
     }
+
+    static void testDataTodoList() {
+        tambahDataTodo("Satu");
+        tambahDataTodo("Dua");
+        tambahDataTodo("Tiga");
+        tambahDataTodo("Empat");
+        tambahDataTodo("Lima");
+
+        var result = hapusDataTodo(20);
+        System.out.println(result);
+
+        result = hapusDataTodo(7);
+        System.out.println(result);
+
+        result = hapusDataTodo(1);
+        System.out.println(result);
+
+        tampilkanDataTodo();
+
+    }
+
+    static String input(String info) {
+        System.out.print(info + " : ");
+        String data = scanner.nextLine();
+        return data;
+    }
+
+    static void testInput() {
+        var name = input("Masukkan Data");
+        System.out.println("Data yg Kamu Masukkan  " + name);
+    }
+
+    static void tampilanTodoList() {
+        while (true) {
+            System.out.println("APLIKASI TODOLIST BUTOCAKIL");
+            tampilkanDataTodo();
+            System.out.println("Aplikasi Todo List");
+            System.out.println("1. Tambah Data");
+            System.out.println("2. Hapus Data");
+            System.out.println("3. Keluar");
+
+            var Pilihan = input("Pilih");
+
+            if (Pilihan.equals("1")) {
+                tampilkanlTambahData();
+            } else if (Pilihan.equals("2")) {
+                tampilkanHapusData();
+            } else if (Pilihan.equals("3")) {
+                break;
+            } else {
+                System.out.println("Pilihan Tidak Ada ");
+            }
+        }
+
+    }
+
+    static void testTampilanTodoList() {
+        tambahDataTodo("Satu");
+        tambahDataTodo("Dua");
+        tambahDataTodo("Tiga");
+        tambahDataTodo("Empat");
+        tambahDataTodo("Lima");
+        tampilanTodoList();
+    }
+
+    static void tampilkanlTambahData() {
+        System.out.println("Masukkan TODOLIST");
+
+        var todo = input("Masukkan Data : (x Jika Batal)");
+
+        if (todo.equals("x")) {
+            System.out.println("BATALL");
+        } else {
+            tambahDataTodo(todo);
+        }
+    }
+
+    static void testtampilkanlTambahData() {
+        tambahDataTodo("Satu");
+        tambahDataTodo("Dua");
+
+        tampilanTodoList();
+
+        tampilkanDataTodo();
+    }
+
+    static void tampilkanHapusData() {
+        System.out.println("MENGHAPUS TODOLIST");
+
+        var number = input("Nomor yang Dihapus (x Jika Batal)");
+
+        if (number.equals("x")) {
+            System.out.println("Batal Lagi");
+        } else {
+
+            boolean success = hapusDataTodo(Integer.valueOf(number));
+            if (!success) {
+                System.out.println("Gagal menghapus todolist : " + number);
+            }
+        }
+    }
+
+    static void testtampilkanHapusData() {
+        tambahDataTodo("Satu");
+        tambahDataTodo("Dua");
+        tambahDataTodo("Tiga");
+
+        tampilanTodoList();
+
+        tampilkanHapusData();
+
+    }
+
 }
